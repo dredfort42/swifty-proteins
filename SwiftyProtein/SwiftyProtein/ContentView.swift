37bd11a8bbd0b15c28d8a738464a95a0775a47f7
Dmitry Novikov <dredfort.42@gmail.com>
2022-11-15 14:14:07 +0300

    ican and Launchscreen added

--- SwiftyProtein/SwiftyProtein/ContentView.swift
+++ SwiftyProtein/SwiftyProtein/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+			Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+			Spacer()
             Text("Hello, world!")
+			Spacer()
         }
         .padding()
     }
