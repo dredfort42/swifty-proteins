2e2021ba7068b0d8e5639342029e229e46089135
Dmitry Novikov <dredfort.42@gmail.com>
2022-11-15 12:11:28 +0300

    let's start

---
+++ SwiftyProtein/SwiftyProtein/SwiftyProteinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyProteinApp.swift
+//  SwiftyProtein
+//
+//  Created by Dmitry Novikov on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyProteinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
