6df8377e33688e995f6e674c267eb68df514c19a
Dmitry Novikov <dredfort.42@gmail.com>
2022-11-24 12:28:16 +0300

    FaceID button animation added

--- SwiftyProtein/SwiftyProtein/Views/LoginView.swift
+++ SwiftyProtein/SwiftyProtein/Views/LoginView.swift
@@ -11,42 +11,58 @@ struct LoginView: View {
 	var body: some View {
 		VStack {
 			Spacer()
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundColor(.black)
-			Text("Hello, world!")
-				.padding(21)
-			ZStack {
-				Circle()
-					.frame(width: 100, height: 100)
-					.foregroundColor(.orange)
-				Circle()
-					.frame(width: 92, height: 92)
-					.foregroundColor(.white)
-				Button(
-					action: authentication,
-					label: {Image(systemName: "faceid")
-							.resizable()
-							.foregroundColor(.orange)
-							.frame(width: 60, height: 60)
-					}
-				)
-			}
-
+			AuthenticationButton()
 			Spacer()
 		}
+		.background(
+			Image("Background")
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.edgesIgnoringSafeArea(.all)
+				.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+		)
 	}
-
-	//MARK: - Functions
-
-	func authentication() {
-		print("Button")
-	}
-
 }
 
 struct LoginView_Previews: PreviewProvider {
 	static var previews: some View {
 		LoginView()
+	}
+}
+
+//MARK: - Button view
+
+struct AuthenticationButton: View {
+	@State var buttonTapped = false
+	@State var buttonPressed = false
+
+	var body: some View {
+		ZStack {
+			Image(systemName: "faceid")
+				.font(.system(size: 64, weight: .thin))
+		}
+		.frame(width: 100, height: 100)
+		.background(
+			ZStack {
+				Circle()
+					.fill(.white)
+					.frame(width: 120, height: 120)
+					.shadow(color: Color(.sRGBLinear, red: 1.0, green: 0.0, blue: 0.0, opacity: 0.2), radius: 2, x: -2, y: 3)
+					.shadow(color: Color(.sRGBLinear, red: 0.0, green: 1.0, blue: 0.0, opacity: 0.2), radius: 2, x: -2, y: -3)
+					.shadow(color: Color(.sRGBLinear, red: 0.0, green: 0.0, blue: 1.0, opacity: 0.2), radius: 2, x: 2, y: 3)
+			}
+		)
+		.scaleEffect(buttonTapped ? 0.95 : 1)
+		.onTapGesture() {
+			authentication()
+			buttonTapped.toggle()
+			DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+				buttonTapped = false
+			}
+		}
+	}
+
+	func authentication() {
+		print("Button")
 	}
 }
