38bde217024923aad557ba8a8ccc7aea6819c344
Dmitry Novikov <dredfort.42@gmail.com>
2022-11-27 18:59:48 +0300

    Login View Controller part done

--- SwiftyProtein/SwiftyProtein/Views/LoginView.swift
+++ SwiftyProtein/SwiftyProtein/Views/LoginView.swift
@@ -10,14 +10,16 @@ import LocalAuthentication
 
 struct LoginView: View {
 
+	let context = LAContext()
+
 	@State private var successAuth = false
 	@State private var buttonTapped = false
-
+	
 	var body: some View {
-		if successAuth {
+		if !checkAuthWithBiometrics() || successAuth {
 			ProteinListView()
 		} else {
-			AuthenticationButton()
+			AuthenticationButtonView()
 				.scaleEffect(buttonTapped ? 0.95 : 1)
 				.onTapGesture() {
 					buttonTapped.toggle()
@@ -33,76 +35,38 @@ struct LoginView: View {
 						.edgesIgnoringSafeArea(.all)
 						.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
 				)
+			
 		}
 	}
 
-	func authentication() {
-
-		let context = LAContext()
+	func checkAuthWithBiometrics() -> Bool {
 		var error: NSError?
 
-		// check whether biometric authentication is possible
 		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-			// it's possible, so go ahead and use it
-			let reason = "We need to unlock your data."
-
-			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-				// authentication has now completed
+			return true
+		}
+		print("NO BIOMETRICS")
+		return false
+	}
+	
+	func authentication() {
+		let reason = "We need to unlock your data."
+		
+		context.evaluatePolicy(
+			.deviceOwnerAuthenticationWithBiometrics,
+			localizedReason: reason) { success, authenticationError in
 				if success {
-					// authenticated successfully
-					print("SUCCESS")
+					print("BIOMETRICS SUCCESS")
 					successAuth.toggle()
 				} else {
-					// there was a problem
-					print("FAULSE")
+					print("BIOMETRICS FAULSE")
 				}
 			}
-		} else {
-			// no biometrics
-			print("NO BIOMETRICS")
-		}
 	}
 }
 
 struct LoginView_Previews: PreviewProvider {
 	static var previews: some View {
 		LoginView()
-	}
-}
-
-//MARK: - Button view
-
-struct AuthenticationButton: View {
-
-	@State private var buttonRotating = 0.0
-
-	var body: some View {
-		ZStack {
-			Image(systemName: "faceid")
-				.font(.system(size: 64, weight: .thin))
-				.foregroundColor(Color(white: 0.2))
-		}
-		.frame(width: 100, height: 100)
-		.background(
-			ZStack {
-				Circle()
-					.fill(Color(white: 0.95))
-					.frame(width: 120, height: 120)
-					.shadow(color: .cyan.opacity(0.1), radius: 1, x: 0, y: -10)
-					.shadow(color: .yellow.opacity(0.1), radius: 1, x: 0, y: 10)
-					.shadow(color: .purple.opacity(0.1), radius: 1, x: 10, y: 0)
-					.shadow(color: .green.opacity(0.1), radius: 1, x: -10, y: 0)
-					.rotationEffect(.degrees(buttonRotating))
-					.onAppear {
-						withAnimation(.linear(duration: 1)
-							.speed(0.1).repeatForever(autoreverses: false)) {
-								buttonRotating = 360.0
-							}
-					}
-				Circle()
-					.fill(.white)
-					.frame(width: 118, height: 118)
-			}
-		)
 	}
 }
--- SwiftyProtein/SwiftyProtein/Views/ProteinListView.swift
+++ SwiftyProtein/SwiftyProtein/Views/ProteinListView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ProteinListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+	@State private var layerTrancparency = 0.0
+
+
+	var body: some View {
+
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+			.opacity(layerTrancparency)
+			.onAppear {
+				withAnimation(.easeIn(duration: 1.0)) {
+					layerTrancparency = 1.0
+				}
+			}
+
+	}
+	
 }
 
 struct ProteinListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProteinListView()
-    }
+	static var previews: some View {
+		ProteinListView()
+	}
 }
